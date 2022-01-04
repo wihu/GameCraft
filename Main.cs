5142209a1d5ebf4550f92f11ef316aa91971c197
wihu <william.hutama@gmail.com>
2022-01-04 20:24:10 +0100

    Initial project setup.

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace GameCraftMac
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
