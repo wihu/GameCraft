132fba737cc8e1506994fae838e8564d27f5a8db
wihu <william.hutama@gmail.com>
2022-01-24 15:38:50 +0100

    Added test render background texture using sprite batch and handle view resizing.

--- GameCraft.Views/GameView.cs
+++ GameCraft.Views/GameView.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Drawing;
 using Foundation;
 using AppKit;
 using CoreGraphics;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform.MacOS;
 using OpenGL;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-//using MonoGame.OpenGL;
+using Microsoft.Xna.Framework.Content;
 
 namespace GameCraft.Views
 {
-	public class GameView : OpenTK.Platform.MacOS.MonoMacGameView, IDisposable
+	public class GameView : MonoMacGameView, IDisposable, IGraphicsDeviceService
 	{
-		float rtri; // Angle For The Triangle 
-		float rquad; // Angle For The Quad
+		public GraphicsDevice GraphicsDevice => _graphicsDevice;
+
+		public event EventHandler<EventArgs> DeviceCreated;
+		public event EventHandler<EventArgs> DeviceDisposing;
+		public event EventHandler<EventArgs> DeviceReset;
+		public event EventHandler<EventArgs> DeviceResetting;
+
 		private bool _isPaused;
+		private Viewport _viewport;
 		private GraphicsDevice _graphicsDevice;
+		private GameServiceContainer _services;
+		private ContentManager _content;
+		private SpriteBatch _spriteBatch;
+		private Texture2D _background;
 
 		[Export("initWithFrame:")]
 		public GameView(CGRect frame) : this(frame, null)
@@ -25,31 +36,7 @@ namespace GameCraft.Views
 
 		public GameView(CGRect frame, NSOpenGLContext context) : base(frame, context)
         {
-			Console.WriteLine("GameView INIT");
-			Resize += delegate {
-				//ResizeGL(Bounds);
-            };
-			Load += delegate(object src, EventArgs args)
-			{
-				//InitGL();
-				UpdateView();
-			};
-
-			RenderFrame += delegate (object src, FrameEventArgs args)
-			{
-				if (this.OpenGLContext.View == null)
-                {
-					Console.WriteLine("Render");
-					this.OpenGLContext.View = this;
-                }
-
-				// render scene
-				if (_graphicsDevice != null)
-                {
-					_graphicsDevice.Clear(Microsoft.Xna.Framework.Color.CornflowerBlue);
-				}
-				//DrawScene();
-			};
+			Initialize();
         }
 
 		~GameView()
@@ -93,168 +80,72 @@ namespace GameCraft.Views
 			NSNotificationCenter.DefaultCenter.RemoveObserver(NSWindow.DidEndLiveResizeNotification);
 		}
 
-        protected override void OnResize(EventArgs e)
+        void HandleResize(object src, EventArgs args)
         {
-            base.OnResize(e);
-			Console.WriteLine("Resize");
+			if (_graphicsDevice == null)
+            {
+				return;
+            }
+			Console.WriteLine("Resize" + this.Size);
+			if (this.Size.Width != _viewport.Width || this.Size.Height != _viewport.Height)
+            {
+				_graphicsDevice.PresentationParameters.BackBufferWidth = _viewport.Width = this.Size.Width;
+				_graphicsDevice.PresentationParameters.BackBufferHeight = _viewport.Height = this.Size.Height;
+				_graphicsDevice.Viewport = _viewport;
+			}
         }
+
+		void Initialize()
+        {
+			Console.WriteLine("GameView INIT");
+
+			Resize += HandleResize;
+			Load += delegate (object src, EventArgs args)
+			{
+				UpdateView();
+			};
+
+			RenderFrame += delegate (object src, FrameEventArgs args)
+			{
+				if (this.OpenGLContext.View == null)
+				{
+					Console.WriteLine("Render");
+					this.OpenGLContext.View = this;
+				}
+
+				RenderScene();
+			};
+		}
 
 		void InitializeGraphicsDevice()
 		{
 			// init graphics device
+			_viewport = new Viewport(0, 0, this.Size.Width, this.Size.Height);
+
 			var presParams = new PresentationParameters();
+			presParams.BackBufferWidth = _viewport.Width;
+			presParams.BackBufferHeight = _viewport.Height;
+
 			_graphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter, GraphicsProfile.Reach, presParams);
+
+			_services = new GameServiceContainer();
+			_services.AddService<IGraphicsDeviceService>(this);
+			_content = new ContentManager(_services, "Content");
+			_spriteBatch = new SpriteBatch(_graphicsDevice);
+			_background = _content.Load<Texture2D>("Layer1_0");
 		}
 
-        private void InitGL()
-		{
-			// Enables Smooth Shading  
-			GL.ShadeModel(ShadingModel.Smooth);
-			// Set background color to black     
-			GL.ClearColor(Color.Black);
-
-			// Setup Depth Testing
-
-			// Depth Buffer setup
-			GL.ClearDepth(1.0);
-			// Enables Depth testing
-			GL.Enable(EnableCap.DepthTest);
-			// The type of depth testing to do
-			GL.DepthFunc(DepthFunction.Lequal);
-
-			// Really Nice Perspective Calculations
-			GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
-		}
-
-		private void ResizeGL(CGRect bounds)
-		{
-			// Reset The Current Viewport
-			GL.Viewport(0, 0, (int)bounds.Size.Width, (int)bounds.Size.Height);
-			// Select The Projection Matrix
-			GL.MatrixMode(MatrixMode.Projection);
-			// Reset The Projection Matrix
-			GL.LoadIdentity();
-
-			// Set perspective here - Calculate The Aspect Ratio Of The Window
-			Perspective(45, bounds.Size.Width / bounds.Size.Height, 0.1, 100);
-
-			// Select The Modelview Matrix
-			GL.MatrixMode(MatrixMode.Modelview);
-			// Reset The Modelview Matrix
-			GL.LoadIdentity();
-		}
-
-		// This creates a symmetric frustum.
-		// It converts to 6 params (l, r, b, t, n, f) for glFrustum()
-		// from given 4 params (fovy, aspect, near, far)
-		private static void Perspective(double fovY, double aspectRatio, double front, double back)
-		{
-			const
-			double DEG2RAD = Math.PI / 180;
-
-			// tangent of half fovY
-			double tangent = Math.Tan(fovY / 2 * DEG2RAD);
-
-			// half height of near plane
-			double height = front * tangent;
-
-			// half width of near plane
-			double width = height * aspectRatio;
-
-			// params: left, right, bottom, top, near, far
-			GL.Frustum(-width, width, -height, height, front, back);
-		}
-
-		private bool DrawScene()
-		{
-			// Clear The Screen And The Depth Buffer
-			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			// Reset The Current Modelview Matrix
-			GL.LoadIdentity();
-
-			GL.Translate(-1.5f, 0.0f, -6.0f);
-			// Move Left 1.5 Units And Into The Screen 6.0
-			GL.Rotate(rtri, 0.0f, 1.0f, 0.0f);
-			// Rotate The Triangle On The Y axis
-			GL.Begin(BeginMode.Triangles);      // Start drawing the Pyramid
-
-			GL.Color3(1.0f, 0.0f, 0.0f);            // Red
-			GL.Vertex3(0.0f, 1.0f, 0.0f);           // Top Of Triangle (Front)
-			GL.Color3(0.0f, 1.0f, 0.0f);            // Green
-			GL.Vertex3(-1.0f, -1.0f, 1.0f);         // Left Of Triangle (Front)
-			GL.Color3(0.0f, 0.0f, 1.0f);            // Blue
-			GL.Vertex3(1.0f, -1.0f, 1.0f);          // Right Of Triangle (Front)
-
-			GL.Color3(1.0f, 0.0f, 0.0f);            // Red
-			GL.Vertex3(0.0f, 1.0f, 0.0f);           // Top Of Triangle (Right)
-			GL.Color3(0.0f, 0.0f, 1.0f);            // Blue
-			GL.Vertex3(1.0f, -1.0f, 1.0f);          // Left Of Triangle (Right)
-			GL.Color3(0.0f, 1.0f, 0.0f);            // Green
-			GL.Vertex3(1.0f, -1.0f, -1.0f);         // Right Of Triangle (Right)
-
-			GL.Color3(1.0f, 0.0f, 0.0f);            // Red
-			GL.Vertex3(0.0f, 1.0f, 0.0f);           // Top Of Triangle (Back)
-			GL.Color3(0.0f, 1.0f, 0.0f);            // Green
-			GL.Vertex3(1.0f, -1.0f, -1.0f);         // Left Of Triangle (Back)
-			GL.Color3(0.0f, 0.0f, 1.0f);            // Blue
-			GL.Vertex3(-1.0f, -1.0f, -1.0f);            // Right Of Triangle (Back)			
-
-			GL.Color3(1.0f, 0.0f, 0.0f);            // Red
-			GL.Vertex3(0.0f, 1.0f, 0.0f);           // Top Of Triangle (Left)
-			GL.Color3(0.0f, 0.0f, 1.0f);            // Blue
-			GL.Vertex3(-1.0f, -1.0f, -1.0f);            // Left Of Triangle (Left)
-			GL.Color3(0.0f, 1.0f, 0.0f);            // Green
-			GL.Vertex3(-1.0f, -1.0f, 1.0f);         // Right Of Triangle (Left)
-
-			GL.End();                       // Finished Drawing The Pyramid
-
-			// Reset The Current Modelview Matrix
-			GL.LoadIdentity();
-
-			GL.Translate(1.5f, 0.0f, -7.0f);            // Move Right 1.5 Units And Into The Screen 7.0
-			GL.Rotate(rquad, 1.0f, 0.0f, 0.0f);         // Rotate The Quad On The X axis ( NEW )   
-			GL.Begin(BeginMode.Quads);              // Start Drawing Cube
-			GL.Color3(0.0f, 1.0f, 0.0f);            // Set The Color To Green
-			GL.Vertex3(1.0f, 1.0f, -1.0f);          // Top Right Of The Quad (Top)
-			GL.Vertex3(-1.0f, 1.0f, -1.0f);     // Top Left Of The Quad (Top)
-			GL.Vertex3(-1.0f, 1.0f, 1.0f);          // Bottom Left Of The Quad (Top)
-			GL.Vertex3(1.0f, 1.0f, 1.0f);           // Bottom Right Of The Quad (Top)                        
-
-			GL.Color3(1.0f, 0.5f, 0.0f);            // Set The Color To Orange
-			GL.Vertex3(1.0f, -1.0f, 1.0f);          // Top Right Of The Quad (Bottom)
-			GL.Vertex3(-1.0f, -1.0f, 1.0f);     // Top Left Of The Quad (Bottom)
-			GL.Vertex3(-1.0f, -1.0f, -1.0f);        // Bottom Left Of The Quad (Bottom)
-			GL.Vertex3(1.0f, -1.0f, -1.0f);         // Bottom Right Of The Quad (Bottom)
-
-			GL.Color3(1.0f, 0.0f, 0.0f);            // Set The Color To Red
-			GL.Vertex3(1.0f, 1.0f, 1.0f);           // Top Right Of The Quad (Front)
-			GL.Vertex3(-1.0f, 1.0f, 1.0f);          // Top Left Of The Quad (Front)
-			GL.Vertex3(-1.0f, -1.0f, 1.0f);         // Bottom Left Of The Quad (Front)
-			GL.Vertex3(1.0f, -1.0f, 1.0f);          // Bottom Right Of The Quad (Front)	
-
-			GL.Color3(1.0f, 1.0f, 0.0f);            // Set The Color To Yellow
-			GL.Vertex3(1.0f, -1.0f, -1.0f);     // Bottom Left Of The Quad (Back)
-			GL.Vertex3(-1.0f, -1.0f, -1.0f);        // Bottom Right Of The Quad (Back)
-			GL.Vertex3(-1.0f, 1.0f, -1.0f);     // Top Right Of The Quad (Back)
-			GL.Vertex3(1.0f, 1.0f, -1.0f);      // Top Left Of The Quad (Back)
-
-			GL.Color3(0.0f, 0.0f, 1.0f);            // Set The Color To Blue
-			GL.Vertex3(-1.0f, 1.0f, 1.0f);          // Top Right Of The Quad (Left)
-			GL.Vertex3(-1.0f, 1.0f, -1.0f);         // Top Left Of The Quad (Left)
-			GL.Vertex3(-1.0f, -1.0f, -1.0f);            // Bottom Left Of The Quad (Left)
-			GL.Vertex3(-1.0f, -1.0f, 1.0f);         // Bottom Right Of The Quad (Left)
-
-			GL.Color3(1.0f, 0.0f, 1.0f);            // Set The Color To Violet
-			GL.Vertex3(1.0f, 1.0f, -1.0f);          // Top Right Of The Quad (Right)
-			GL.Vertex3(1.0f, 1.0f, 1.0f);           // Top Left Of The Quad (Right)
-			GL.Vertex3(1.0f, -1.0f, 1.0f);          // Bottom Left Of The Quad (Right)
-			GL.Vertex3(1.0f, -1.0f, -1.0f);         // Bottom Right Of The Quad (Right)			
-
-			GL.End();               // Done Drawing the Cube
-
-			rtri += 2f;             // Increase The Rotation Variable For The Triangle
-			rquad -= 0.15f;             // Decrease The Rotation Variable For The Quad 
-			return true;
+		void RenderScene()
+        {
+			if (_graphicsDevice == null)
+			{
+				return;
+			}
+			_graphicsDevice.Clear(Color.CornflowerBlue);
+			_spriteBatch.Begin();
+			var rect = new Rectangle(0, 0, _viewport.Width, _viewport.Height);
+			_spriteBatch.Draw(_background, rect, Color.White);
+			_spriteBatch.End();
 		}
 	}
 }
